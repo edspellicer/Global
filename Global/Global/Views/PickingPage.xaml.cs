b61cd69993be0d620f7adf4dcbd98d2f5f0f029c
Estefania DSantiago <estefaniadsantiago@gmail.com>
2020-07-28 11:09:02 -0400

    28-07-2020

--- Global/Global/Views/PickingPage.xaml.cs
+++ Global/Global/Views/PickingPage.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Global.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +15,20 @@ namespace Global.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PickingPage : ContentPage
 	{
-		public PickingPage ()
+        private const string Url = "http://192.168.0.62:8082/api/PedidosAsignados";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<PedidosAsignados> _pedidoasignado;
+        public PickingPage ()
 		{
 			//InitializeComponent ();
 		}
-	}
+        protected override async void OnAppearing()
+        {
+            string content = await client.GetStringAsync(Url);
+            List<Lgperson> lgperson = JsonConvert.DeserializeObject<List<Lgperson>>(content);
+            //_pedidoasignado = new ObservableCollection<PedidosAsignados>(pedidoasignado);
+            //MyListViewpa.ItemsSource = _pedidoasignado;
+            base.OnAppearing();
+        }
+    }
 }
